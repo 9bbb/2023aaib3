68ae06f07ebe7165882abb9b5eb7d571df5f3ca2
9bbb <145088325+9bbb@users.noreply.github.com>
2024-10-16 10:55:46 +0800

    Add files via upload

---
+++ w6/w6-2.-a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int>a;
+    int now;
+    while( cin>>now){
+        a.push_back(now);
+        int N =a.size();
+        cout << now<"\n";
+
+    }
+
+
+}
