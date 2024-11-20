0a85b46f0490d730a26c34bca665b3cdcf36b474
9bbb <145088325+9bbb@users.noreply.github.com>
2024-11-20 11:36:02 +0800

    week11

---
+++ w11/w11-1.py
@@ -0,0 +1,7 @@
+table={} #python用大瓜號 代表字典 也就是Hash Table(Hash Map,Hash Set)
+table[9977341]=9
+table[4433997]=4
+print(table[9977341])
+print(table[4433997])
+if 0 not in table:
+  print("table[0]不存在")
